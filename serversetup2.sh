d37103116a6b27e4c153b97fcbe6ca85e5f6b3b3
Stormle <alvar.stara@gmail.com>
2020-07-08 22:39:00 +0300

    Compose functionality and further automation.

---
+++ serversetup2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo helloworld2
+
+websockify --cert=/home/node/mykey.crt --key=/home/node/mykey.key --ssl-only --ssl-target --web=/home/node/dist 1443 localhost:64738 &
+websockify --ssl-target --web=/home/node/dist 8080 localhost:64738 &
+
+/usr/bin/murmurd -fg -v
